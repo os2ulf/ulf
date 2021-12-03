d0ae55c82cc5705be0c475b84a404ad9823183e6
Søren Due Bonde <sdb@novicell.dk>
2021-12-03 10:03:26 +0100

    OS2ULF-504: Update Drupal from 7.82 to 7.83

---
+++ sites/sites.local.php
@@ -0,0 +1,13 @@
+<?php
+
+$sites['ulfiaarhus.test'] = 'ulfiaarhus.dk';
+$sites['aabenaalborg.test'] = 'aabenaalborg.dk';
+$sites['ude.nu.test'] = 'ude.nu';
+$sites['klcviborg.test'] = 'klcviborg.dk';
+$sites['udoglaer.test'] = 'udoglaer.dk';
+$sites['rum.thisted.test'] = 'rum.thisted.dk';
+$sites['udoglaer.randers.test'] = 'udoglaer.randers.dk';
+$sites['mitvadehav.test'] = 'mitvadehav.dk';
+$sites['ungegarantien.test'] = 'ungegarantien.dk';
+$sites['laeringsambassaden.test'] = 'laeringsambassaden.dk';
+$sites['udoglaer.vejle.test'] = 'udoglaer.vejle.dk';
