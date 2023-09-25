813914c29c576fdc584548c3e20f13e4895ccd67
Søren Due Bonde <sdb@novicell.dk>
2023-09-25 14:15:45 +0200

    OS2ULF-654: Adjusted console.log for debugging on prod

--- profiles/ulf/modules/ulf_custom/ulf_search/js/mapDirective.js
+++ profiles/ulf/modules/ulf_custom/ulf_search/js/mapDirective.js
@@ -49,10 +49,10 @@ angular.module('searchResultApp').directive('searchMap', [ '$timeout', '$templat
     function addMarker(data, scope) {
       return $q(function(resolve, reject) {
         var location = data.field_location;
-        console.log(data);
-        console.log(location);
 
         if (location !== null && location.lat !== null && location.lon !== null) {
+          console.log('data', data);
+          console.log('location', location);
           if (location.lat > 0 && location.lon > 0) {
             var marker = L.marker([location.lat, location.lon]);
 
@@ -112,7 +112,6 @@ angular.module('searchResultApp').directive('searchMap', [ '$timeout', '$templat
         // Watch for changes in hits
         communicatorService.$on('mapSearchHits', function onMapHits(event, hits) {
           scope.mapHits = hits;
-          console.log(hits);
 
           // Add all results to the map.
           var promises = [];
@@ -134,13 +133,15 @@ angular.module('searchResultApp').directive('searchMap', [ '$timeout', '$templat
               // Clean out resolved markers as some are without lat/lon and is
               // undefined in the array.
               var hitCount = 0;
+              console.log('cluster before loop', cluster);
               for (var i = 0; i < data.length; i++) {
-                console.log(data[i]);
                 if (data[i] !== undefined) {
+                  console.log('data[i]', data[i]);
                   hitCount++;
                   cluster.addLayer(data[i]);
                 }
               }
+              console.log('cluster after loop', cluster);
 
               // Update hit counter.
               scope.hitCount = hitCount;
